7bcf816fca8cc386cae173b7ed95f0d3951a16a4
ezeforte <ezequiel3925@hotmail.com>
2021-07-24 01:55:36 -0300

    tercer commit

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -73,7 +73,7 @@ import dropdownCaret from './dropdown-caret.svg';
 import languageIcon from '../language-selector/language-icon.svg';
 import aboutIcon from './icon--about.svg';
 
-//import scratchLogo from './scratch-logo.svg';
+
 import scratchLogo from './CREATIVOS.jpg';
 
 
